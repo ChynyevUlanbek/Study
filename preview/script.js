b2f61e9d2759064aa12a60d1e29becfc15cf8312
Ulanbek <ulanbek89@mail.ru>
2023-09-03 00:01:38 +0600

    First commit

--- preview/script.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const str = 'My name is R2D2';
-
-console.log(str.match(/\w\d\w\d/i));
---
+++ starter/src/App.js
@@ -0,0 +1,92 @@
+import { useState, useEffect, useCallback, useMemo } from 'react';
+import {Container} from 'react-bootstrap';
+import './App.css';
+
+const countTotal = (num) => {
+    console.log('counting...');
+    return num + 10;
+}
+
+const Slider = (props) => {
+
+    const [slide, setSlide] = useState(0);
+    const [autoplay, setAutoplay] = useState(false);
+
+    const getSomeImages = useCallback(() => {
+        console.log('fetching')
+        return [
+            "https://a.cdn-hotels.com/gdcs/production153/d1371/e6c1f55e-51ac-41d5-8c63-2d0c63faf59e.jpg",
+            "https://a.cdn-hotels.com/gdcs/production29/d1870/6a5ec560-bb25-11e8-970b-0242ac110006.jpg?impolicy=fcrop&w=800&h=533&q=medium"
+        ]
+    }, [slide]);
+
+    function changeSlide(i) {
+        setSlide(slide => slide + i);
+    }
+
+    function toggleAutoplay() {
+        setAutoplay(autoplay => !autoplay);
+    }
+
+    const total = useMemo(() => {
+        return countTotal(slide);
+    }, [slide]);
+
+    const style = useMemo(() => ({
+        color: slide > 4 ? 'red' : 'black'
+    }), [slide]);
+
+    useEffect(() => {
+        console.log('style')
+    }, [style])
+
+    return (
+        <Container>
+            <div className="slider w-50 m-auto">
+            <Slide getSomeImages={getSomeImages}/>
+
+                <div className="text-center mt-5">Active slide {slide} <br/>{autoplay ? 'auto' : null}</div>
+                <div style={style} className="text-center mt-5">Total slides: {total}</div>
+                <div className="buttons mt-3">
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => changeSlide(-1)}>-1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => changeSlide(1)}>+1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={toggleAutoplay}>toggle autoplay</button>
+                </div>
+            </div>
+        </Container>
+    )
+}
+
+const Slide = ({getSomeImages}) => {
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        setImages(getSomeImages())
+    }, [getSomeImages])
+
+    return (
+        <>
+        {images.map((url, i) => <img key={i} className="d-block w-100" src={url} alt="slide" />)}
+        </>
+    )
+}
+
+
+function App() {
+    const [slider, setSlider] = useState(true);
+
+  return (
+    <>
+    <button onClick={() => setSlider(false)}>Click</button>
+    {slider ? <Slider/> : null}
+    </>
+  );
+}
+
+export default App;
